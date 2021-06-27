dd4a125116fa03efafb1f1081cc07e31d1f29948
Michelle B <86215527+MichCS@users.noreply.github.com>
2021-06-27 16:32:22 +0000

    added pre05 Triangle.java

--- pre02/Time.java
+++ pre02/Time.java
@@ -1,35 +1,18 @@
-import java.io.*;
-import java.util.*;
+public class Triangle {
+    public static void main(String args[]) {
 
-public class Time {
-    public static void main(String[] args){
-   
-int hour = 13;
-int minute = 8;
-int second = 03;
-
-System.out.println("The amount of seconds that have passed since midnight:" + " " + ((hour * 60)*60) + " " + "seconds");
-System.out.println();
-System.out.println("The amount of seconds that remain in the day:" + " " + (((24 - hour)*60)*60) +  " " + "seconds");
-System.out.println();
-
-		double Hour = 13.0;
-		double percentage = ((hour/24.0)*100);
-		int CurrentHour = 13;
-		int CurrentMinute = 45;
-		int CurrentSecond = 05;
-		
-		
-		
-		System.out.println("The amount of seconds that have passed since midnight:" + " " + ((hour * 60)*60) + " " + "seconds");
-    System.out.println();
-		System.out.println("The amount of seconds that remain in the day:" + " " + (((24 - hour)*60)*60) +  " " + "seconds"); 
-    System.out.println();
-		System.out.println("The percentage of the day that has passed: " + percentage + "%");
-    System.out.println();
-		System.out.println("The elapsed time since working on this exercise: " + (CurrentHour - Hour) + ":" + (CurrentMinute - minute) + ":" + (CurrentSecond - second)); 
-		
-		
+      System.out.println(isTriangle(2,4,6));
+    
     }
-}
 
+     public static boolean isTriangle(int a, int b, int c){
+        boolean triangle = true;
+        if(a+b < c || a+c < b || b+c < a){
+            triangle = false;
+     
+        } else {
+            triangle = true;
+        } 
+        return triangle;
+    } 
+}
